5f3986d0dfa5be906cd60052cc593e82b8db920b
helping-bored-children-in-school <lorax.trees.sthe@gmail.com>
2024-12-15 06:17:05 +0000

    Add files via upload

---
+++ js/ruffle/ruffle-player.js
@@ -0,0 +1,10 @@
+window.RufflePlayer = window.RufflePlayer || {};
+window.RufflePlayer.config = {
+    warnOnUnsupportedContent: false,
+    autoplay: "on",
+    unmuteOverlay: "hidden",
+    maxExecutionDuration: 30,
+    base: null,
+    forceScale: false,
+    quality: 'high'
+};
