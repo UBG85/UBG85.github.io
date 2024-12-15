3186f87a60b53123687003ae2583c3f2536f6e66
helping-bored-children-in-school <lorax.trees.sthe@gmail.com>
2024-12-15 08:04:26 +0000

    Create solitare.js

---
+++ js/solitare.js
@@ -0,0 +1,58 @@
+var container = document.querySelector("#unity-container");
+      var canvas = document.querySelector("#unity-canvas");
+      var loadingBar = document.querySelector("#unity-loading-bar");
+      var progressBarFull = document.querySelector("#unity-progress-bar-full");
+      var fullscreenButton = document.querySelector("#unity-fullscreen-button");
+      var warningBanner = document.querySelector("#unity-warning");
+
+      function unityShowBanner(msg, type) {
+        function updateBannerVisibility() {
+          warningBanner.style.display = warningBanner.children.length ? 'block' : 'none';
+        }
+        var div = document.createElement('div');
+        div.innerHTML = msg;
+        warningBanner.appendChild(div);
+        if (type == 'error') div.style = 'background: red; padding: 10px;';
+        else {
+          if (type == 'warning') div.style = 'background: yellow; padding: 10px;';
+          setTimeout(function() {
+            warningBanner.removeChild(div);
+            updateBannerVisibility();
+          }, 5000);
+        }
+        updateBannerVisibility();
+      }
+
+      var buildUrl = "Solitare";
+      var loaderUrl = buildUrl + "/v03 WebGL.loader.js";
+      var config = {
+        dataUrl: buildUrl + "/v03 WebGL.data",
+        frameworkUrl: buildUrl + "/v03 WebGL.framework.js",
+        codeUrl: buildUrl + "/v03 WebGL.wasm",
+        streamingAssetsUrl: "StreamingAssets",
+        companyName: "DefaultCompany",
+        productName: "Pigeon Post",
+        productVersion: "1.0",
+        showBanner: unityShowBanner,
+      };
+      
+      canvas.style.width = "960px";
+        canvas.style.height = "600px";
+
+      loadingBar.style.display = "block";
+
+      var script = document.createElement("script");
+      script.src = loaderUrl;
+      script.onload = () => {
+        createUnityInstance(canvas, config, (progress) => {
+          progressBarFull.style.width = 100 * progress + "%";
+        }).then((unityInstance) => {
+          loadingBar.style.display = "none";
+          fullscreenButton.onclick = () => {
+            unityInstance.SetFullscreen(1);
+          };
+        }).catch((message) => {
+          alert(message);
+        });
+      };
+      document.body.appendChild(script);
